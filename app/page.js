976a636009720342d73ff690dddff0fd439b4513
Kumail Ahmad <kumailahmad217@gmail.com>
2024-12-29 16:55:42 +0530

    modification

--- app/page.js
+++ app/page.js
@@ -1,6 +1,14 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
   console.log("Rendered & compiled succesfully ");
-  return <div>i am homepage</div>;
+  let data = await fetch("https://jsonplaceholder.typicode.com/photos");
+  let posts = await data.json();
+  return (
+    <ul>
+      {posts.map((post) => {
+        <li key={post.that}>{post.title} </li>;
+      })}
+    </ul>
+  );
 }
