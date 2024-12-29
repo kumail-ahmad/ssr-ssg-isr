4ef5f6c260e6cef8fc2b7275c604afef9049f1ce
Kumail Ahmad <kumailahmad217@gmail.com>
2024-12-29 17:02:34 +0530

    used next-fetch

--- app/page.js
+++ app/page.js
@@ -3,7 +3,9 @@ import React from "react";
 export default async function Home() {
   console.log("Rendered & compiled successfully");
 
-  let data = await fetch("https://jsonplaceholder.typicode.com/photos");
+  let data = await fetch("https://jsonplaceholder.typicode.com/photos", {
+    next: 3600,
+  });
   let posts = await data.json();
 
   return (
