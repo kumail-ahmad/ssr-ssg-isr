9f7fc0137269a349c9a7ce53ff671d5888f6bc8c
Kumail Ahmad <kumailahmad217@gmail.com>
2024-12-29 17:00:29 +0530

    fetched data from api

--- app/page.js
+++ app/page.js
@@ -1,14 +1,20 @@
-import Image from "next/image";
+import React from "react";
 
 export default async function Home() {
-  console.log("Rendered & compiled succesfully ");
+  console.log("Rendered & compiled successfully");
+
   let data = await fetch("https://jsonplaceholder.typicode.com/photos");
   let posts = await data.json();
+
   return (
     <ul>
-      {posts.map((post) => {
-        <li key={post.that}>{post.title} </li>;
-      })}
+      {posts.map((post) => (
+        <ul key={post.id}>
+          <li>
+            {post.title}---------{post.url}{" "}
+          </li>
+        </ul>
+      ))}
     </ul>
   );
 }
